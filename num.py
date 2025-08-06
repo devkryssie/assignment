c886112dde8e73f1ffd4bb46eced1dd063a40364
kryssie3290 <christabeladamu2@gmail.com>
2025-08-06 01:46:55 -0700

    num.py

---
+++ num.py
@@ -0,0 +1,15 @@
+num = 1
+while num <= 1000:
+    print(num)
+    num += 1
+'''
+my_age = 19
+guess = 0
+
+while guess != my_age:
+    guess = int(input("guess christabel age:"))
+    if guess == my_age:
+        print(f"yes christabel is {my_age} years old.....")
+    else:
+        print(f" christabel is not {guess} years old.....")
+   ''' 

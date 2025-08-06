905bb9dc7d03832645794eb0d930d9ab9769790f
kryssie3290 <christabeladamu2@gmail.com>
2025-08-06 01:50:57 -0700

    while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+password = ""
+while password != "111":
+    password = input("enter password:")
+print("access granted")
+
+l_n = 7
+guess = 0
+while guess != l_n:
+    guess = int(input("guess lucky num"))
+print("u guess it right")
+correct_username = "christy"
+correct_pasword = "1234"
+username = ""
+password = ""
+while username != correct_username or password != correct_password:
+    username = input("enter username:")
+    password = input("enter password:")
+    if username != correct_username or password   7 if customer ["loyalty_card"]:!= correct_password:
+        print("invalid login. try again!.\n")
+
+print("login successful")

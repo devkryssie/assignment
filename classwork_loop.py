35c74d2ff4e33b79d8981fceba2d374adbb81caf
kryssie3290 <christabeladamu2@gmail.com>
2025-08-06 01:49:36 -0700

    classwork_loop.py

---
+++ classwork_loop.py
@@ -0,0 +1,30 @@
+age = int(input("enter your age:"))
+if age >= 60:
+    print("can buy ticket")
+    print("senior discount")
+elif age >= 18:
+    print("can buy ticket")
+elif age >= 12:
+    print("teen ticket")
+else:
+    print("kids ticket")
+
+
+budget = float(input("enter your budget:"))
+if budget >= 500:
+    if budget >= 1000:
+        print("we recommend: google pixel 9 pro")
+    else:
+        print("we recommend: iphone")
+elif budget >= 288:
+    print("we recommend: redmi")
+else:
+    print("save more")
+
+
+
+  7 if customer ["loyalty_card"]:
+
+
+
+
